17c06d35b9cff1687033b0849b47b47ca4b6159f
Matheus Dal Mago <matheusdalmago10@hotmail.com>
2020-05-26 19:30:33 -0300

    Task 4

--- Task4.py
+++ Task4.py
@@ -25,3 +25,33 @@ Print a message:
 The list of numbers should be print out one per line in lexicographic order with no duplicates.
 """
 
+
+def number_in_list_element(number, numbers_list, element):
+    for n in numbers_list:
+        if n[element] == number:
+            return True
+    return False
+
+
+def number_in_list(number, numbers_list):
+    for n in numbers_list:
+        if n == number:
+            return True
+    return False
+
+
+mkt_numbers = []
+
+for call in calls:
+    calling_number = call[0]
+
+    if not number_in_list(calling_number, mkt_numbers):
+        if not number_in_list_element(calling_number, calls, 1):  # check for call receiving numbers
+            if not number_in_list_element(calling_number, texts, 0):  # check for text sending numbers
+                if not number_in_list_element(calling_number, texts, 1):  # check for text receiving numbers
+                    mkt_numbers.append(calling_number)
+
+mkt_numbers.sort()
+print("These numbers could be telemarketers:")
+for number in mkt_numbers:
+    print(number)
